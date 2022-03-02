548158845a3e45d3e0647e3ace2cf613c979fd5c
Icarus <520icarus1314@gmail.com>
2022-03-02 10:33:42 +0800

    feat: getQueryParam 从 url 中获取 url 参数的方法

---
+++ utils/url.js
@@ -0,0 +1,8 @@
+const getQueryParam = name => {
+    console.log(location.href)
+    return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.href) || [, ""])[1].replace(/\+/g, '%20')) || null
+}
+
+export {
+    getQueryParam,
+}
