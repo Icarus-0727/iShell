b5f4e546b7fd873232f28e1b96398364962d3afa
Icarus <520icarus1314@gmail.com>
2022-03-02 10:30:07 +0800

    feat: machine-api 增加对后台程序 machine 接口的封装

---
+++ api/machine.js
@@ -0,0 +1,46 @@
+const baseurl = "/machine/";
+
+const getMachines = () =>
+  new Promise((resolve, reject) =>
+    fetch(baseurl)
+      .then((res) => res.json().then(resolve))
+      .catch(reject)
+  );
+
+const getMachine = (id) =>
+  new Promise((resolve, reject) =>
+    fetch(`${baseurl}${id}`)
+      .then((res) => res.json().then(resolve))
+      .catch(reject)
+  );
+
+const addMachine = (machine) =>
+  new Promise((resolve, reject) =>
+    fetch(baseurl, {
+      method: "POST",
+      body: JSON.stringify(machine),
+    })
+      .then((res) => res.json().then(resolve))
+      .catch(reject)
+  );
+
+const updateMachine = (machine) =>
+  new Promise((resolve, reject) =>
+    fetch(baseurl, {
+      method: "PUT",
+      body: JSON.stringify(machine),
+    })
+      .then((res) => res.json().then(resolve))
+      .catch(reject)
+  );
+
+const deleteMachine = (id) =>
+  new Promise((resolve, reject) =>
+    fetch(`${baseurl}${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json().then(resolve))
+      .catch(reject)
+  );
+
+export { getMachines, getMachine, addMachine, updateMachine, deleteMachine };
